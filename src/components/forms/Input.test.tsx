612e0811536dc51ba0de264674b064d9d3be4cfe
Lucas Donghoon Lee <yanggun7201@gmail.com>
2021-09-30 20:58:20 +1300

    Install test libraries and add input test

---
+++ src/components/forms/Input.test.tsx
@@ -0,0 +1,31 @@
+import Input from "./Input";
+import { shallow } from "enzyme";
+
+describe("Input component test", () => {
+    const onFieldChange = jest.fn();
+
+    const inputProps = {
+        name: 'cvc',
+        placeholder: "CVC",
+        value: '',
+        onChange: onFieldChange,
+        type: "number"
+    };
+
+    test("Should fire change event", () => {
+        const output = shallow(<Input {...inputProps} />);
+        const inputComponent = output.find('input');
+        const event = { target: { name: inputProps.name, value: "test" } };
+
+        inputComponent.simulate('change', event);
+        expect(onFieldChange).toHaveBeenCalled();
+    });
+
+    test("Should set type", () => {
+        const output1 = shallow(<Input {...inputProps} />);
+        expect(output1.props()).toHaveProperty('type', 'number');
+
+        const output2 = shallow(<Input {...inputProps} type={"text"} />);
+        expect(output2.props()).toHaveProperty('type', 'text');
+    });
+});
