38f849a3c609d9ede1e129ed4ac100c6fc47d6a4
Lucas Donghoon Lee <yanggun7201@gmail.com>
2021-09-30 19:53:39 +1300

    Apply layouts to Sample menus

--- src/pages/Menu1/index.tsx
+++ src/pages/Menu1/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Container from "../../components/layouts/Container";
+import Title from "../../components/layouts/Title";
+import Content from "../../components/layouts/Content";
 
-const Menu1: React.FC = () => {
-    return (
-        <div>Menu1</div>
-    );
-}
+const Menu1: React.FC = () => (
+    <Container>
+        <Title>Sample Menu 1</Title>
+        <Content>
+            <div>Sample Menu 1</div>
+        </Content>
+    </Container>
+);
 
 export default Menu1;
--- src/pages/Menu2/index.tsx
+++ src/pages/Menu2/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Container from "../../components/layouts/Container";
+import Title from "../../components/layouts/Title";
+import Content from "../../components/layouts/Content";
 
-const Menu2: React.FC = () => {
-    return (
-        <div>Menu2</div>
-    );
-}
+const Menu2: React.FC = () => (
+    <Container>
+        <Title>Sample Menu 1</Title>
+        <Content>
+            <div>Sample Menu 1</div>
+        </Content>
+    </Container>
+);
 
 export default Menu2;
